dd00b17a4d9f92f26a7c7be3f120fd7255e22c87
ElisaMavilia <elisamavilia1@gmail.com>
2025-01-20 17:23:36 +0100

    adjustments

--- src/pages/AboutUsComponent.vue
+++ src/pages/AboutUsComponent.vue
@@ -2,7 +2,7 @@
     <div id="about-us">
       <h1 class="text-center text-uppercase text-general">Om oss</h1>
       <section class="container d-flex justify-content-around align-items-center">
-        <img src="../assets/img/nima.png" alt="Nimas bild">
+            <img src="../assets/img/nima.png" alt="Nimas bild">
         <div class="text-container flex-direction-column">
             <h2>Nima Farasati</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, consectetur. Eligendi, voluptas a laboriosam cum ratione vitae minima architecto fugiat amet quisquam corporis, similique impedit quasi at tenetur cumque, ab perspiciatis quibusdam voluptatibus ex qui molestias sequi sint obcaecati? Molestias quod reprehenderit sint qui consequatur? Ipsa debitis quos explicabo quod dolore ad voluptate</p>
@@ -41,8 +41,15 @@
    #about-us {
     width: 100%;
     background-color: $light_pink;
-   /*  margin-top: 200px; */
+   }
 
+   section {
+    box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
+                inset -7px -7px 10px 0px rgba(0, 0, 0, .1),
+                7px 7px 20px 0px rgba(0, 0, 0, .1),
+                4px 4px 5px 0px rgba(0, 0, 0, .1);
+    margin-top: 100px;
+    margin-bottom: 180px;
    }
 
    img {
